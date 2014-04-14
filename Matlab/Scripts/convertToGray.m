94c43b212c4e77634c2e90ea0094bf24a8279357
Juan Bedoya <jfelipebc@outlook.com>
2014-04-13 19:11:38 -0500

    Archivo para convertir a grises.
    Se suben los pequeños cambios.

--- Matlab/Scripts/convertToGray.m
+++
@@ -1,8 +0,0 @@
-function Image = convertToGray(I)
-[r, c, ch] = size(I); 
-    if(ch == 3)
-        Image = I(:,:,1) * .25 + I(:,:,2) * .5 + I(:,:,3) * .25;
-    else
-        Image = I; 
-    end
-end
--- Matlab/Scripts/detectorImpureza.m
+++ Matlab/Scripts/detectorImpureza.m
@@ -1,21 +1,12 @@
 %Directorio de entrada de los archivos con defectos
 DIRENTRADA = '..\Imagenes\con defecto\';
-DIRENTRADA2 = '..\Imagenes\sin defecto\';
-%Directorios de la salida de los archivos resultantes.
-DIRDEFECTO = '..\Imagenes\Resultados\Defecto\'; 
-DIRSINDEFECTO = '..\Imagenes\Resultados\SinDefecto\';
+
 
 %Elemento estructural
 eleStructural = ones(3); 
 
 files = dir(strcat(DIRENTRADA,'*.bmp'));
 [f c] = size(files);
-
-%Creamos la carpeta que almacena el imperfecto del diente
-mkdir(DIRDEFECTO);
-%Creamos la carpeta que almacena el resultado de quitarle el imperfecto al
-%diente defectuoso. 
-mkdir(DIRSINDEFECTO);
 
 %For que recorre las imágenes y les aplica la funcion Bottom Hat
 %Para pruebas solo se toman 5 imagenes
@@ -24,20 +15,16 @@ for i=1:1
     nomImg = strcat(DIRENTRADA,files(i).name);
     img = imread(nomImg);
     %Convierte la imagen de RGB a escala de grises
-    img = convertToGray(img);
+    img = ConvertToGray(img);
     %Corta la imagen para eliminar, en lo posible, el fondo.
     img = cutImage(img);
     %Aplicamos el filtro Bottom Hat
     bhImagen = imbothat(img,eleStructural);
-    figure,imagesc(bhImagen);
-    %Guardamos la imagen resultante.
-    imwrite(bhImagen, strcat(DIRDEFECTO,files(i).name));
-    %Convertir a escala de grises
-    img = ConvertToGray(img); 
+    figure,imagesc(bhImagen); 
     bhImagen = ConvertToGray(bhImagen);
     %Restar la imagen defecto con la imagen resultante de la operacion
     %Bottom-Hat 
-    resImage = img - bhImagen;
-    figure,imagesc(resImage);
+    %resImage = bhImagen - img;
+    %figure,imagesc(resImage);
 end
 
